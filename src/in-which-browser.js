afb9a9db888e06bcf6dd94383b5edcb6b3640c13
ufologist <klsforever@gmail.com>
2020-02-12 21:34:18 +0800

    docs: 增加分块注释

--- src/in-which-browser.js
+++ src/in-which-browser.js
@@ -7,11 +7,14 @@ import {
 
 import isWeappWebview from './weapp.js';
 
+// UA
 var ua = window.navigator.userAgent;
 var platform = window.navigator.platform;
 
+// Zepto detect
 var result = new Detect(ua, platform);
 
+// extends
 result.browser.wechat = isWechatWebview(ua);
 result.browser.wechatVersion = getWechatVersion(ua);
 result.browser.weapp = isWeappWebview(ua);
